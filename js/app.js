f72ef902a17002c064c290ec0d02cde062fd4ae0
pcruzf1988 <pcruzf1988@gmail.com>
2025-11-11 19:43:43 -0300

    Primer commit - proyecto control de stock

---
+++ js/app.js
@@ -0,0 +1,14 @@
+
+let nombreUsuario = "";
+
+
+function ingresoUsuario() {
+    nombreUsuario = prompt("Hola, bienvenido a Norteña Deco. Ingresa tu nombre para comenzar:   ");
+
+}
+
+ingresoUsuario();
+
+console.log(`Usuario ${nombreUsuario} ha ingresado al sistema.`);
+alert(`Hola ${nombreUsuario}, bienvenido a Norteña Deco. Aquí podrás llevar el control de stock de nuestros productos.`);
+
